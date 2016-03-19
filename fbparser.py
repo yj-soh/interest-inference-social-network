d5bfcde8e0c3110b92ec710cbedef8dbcf4a4b40
yj-soh <soh.you.jun@gmail.com>
2016-03-19 20:15:32 +0800

    Start on FB parser with FB terms removed

---
+++ fbparser.py
@@ -0,0 +1,67 @@
+import codecs
+import os
+import re
+
+FILES = {
+    'training': {
+        'in': 'data/train/Facebook/',
+        'out': 'data/generated/training_fb.txt'
+    },
+    'testing': {
+        'in': 'data/test/Facebook/',
+        'out': 'data/generated/testing_fb.txt'
+    }
+}
+
+options = {
+    'stopwords': False,
+    'force_lowercase': True,
+    'trim_repeat_char': True,
+    'lemma': False,
+    'replace_slang': False,
+    'no_hash_hashtags': True
+}
+
+re_str_date = r'((January|February|March|April|May|June|July|August|September|October|November|December)\s\d+(,\s\d+)?\s)?at\s\d+:\d+(am|pm)'
+re_str_actions = r'added \d+ new photos( to the album:|.)|uploaded a new video.|changed (his|her) profile picture.|likes a link on .+?.|likes an article.|shared a link.|shared .+?\'s photo.|updated (his|her) cover photo.'
+re_str_social = r'\|By|View \d+ more comments?|View \d+ more [replies|reply]|\d+ Comments?|\d+ Likes?|\d+ [S|s]hares?|Share (\d+\speople|.+?) likes? this.'
+re_str_static_terms = r'Automatically Translated  See Original|Comment|Edited|Like|Remove|Reply|See Translation|Share|Water Later|Write a comment... Press Enter to post.|\w+ emoticon'
+
+re_fb_terms = re.compile(re_str_date + '|' + re_str_actions + '|' + re_str_social + '|' + re_str_static_terms, re.UNICODE)
+
+def _parse_text(text):
+    text = re.sub(re_fb_terms, '', text)
+    
+    return text
+
+def _parse_fb(fb_file):
+    with open(fb_file, 'r') as file:
+        text = file.read()
+        text = _parse_text(text)
+        return text
+
+def parse_all_files(new_options=options):
+    dirs = [FILES['training'], FILES['testing']]
+    
+    options = new_options
+    
+    for type in dirs:
+        fb_dir = type['in']
+    
+        # toss everything into memory; should be fine due to data's size
+        
+        files = os.listdir(fb_dir)
+        files = [fn[1:] for fn in files]
+        files = sorted(sorted(files), key=lambda s: len(s))
+        files = ['U' + fn for fn in files]
+        
+        f = codecs.getwriter('utf8')(open(type['out'], 'w'))
+        for file in files:
+            line = _parse_fb(fb_dir + file)
+            line = u' '.join(line.splitlines()) # rejoin item if it contains newline(s)
+            
+            f.write(line + u'\n')
+        f.close()
+
+if __name__ == '__main__':
+        parse_all_files()
